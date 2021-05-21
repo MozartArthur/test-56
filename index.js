4add95aa08b840ef0e55983fd7dce3eb19d7e4dc
MozartArthur <43474724+MozartArthur@users.noreply.github.com>
2021-05-21 05:42:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 (() => {
-  const inputX = document.querySelector("[name=x-fn]");
   const inputY = document.querySelector("[name=y-fn]");
   const submit = document.querySelector("[type=submit]");
   const example = document.querySelector("[type=button]");
@@ -22,7 +21,7 @@
 
   submit.addEventListener("click", event => {
     event.preventDefault();
-    draw(inputX.value, inputY.value);
+    draw(inputY.value);
   });
 
   const examples = [
