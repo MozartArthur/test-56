1b0f1d3bae22bb0b26c277b369527b5a1fed2786
MozartArthur <43474724+MozartArthur@users.noreply.github.com>
2021-05-21 06:19:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
   const renderCartesian = initGl(cartesianGl, unitsPerAxe);
   const renderPolar = initGl(polarGl, unitsPerAxe);
 
-  setAndDraw("0","t");
+  setAndDraw("0","t/2");
 
   submit.addEventListener("click", event => {
     event.preventDefault();
@@ -259,7 +259,6 @@ function drawPolar(exprX, exprY, unitsPerAxe, render) {
   const components = [];
 
     for (let t = -500; t < 500; t += 0.02)  {
-    const angle = exprY.eval({ t });
     const radius = exprY.eval({ t });
 
     if (radius < 0) {
